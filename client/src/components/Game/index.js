aa2094d4eb0ac96bbdafd0b490fa83dc7479c2f5
desguerra <ddesguerra@ucdavis.edu>
2023-01-11 14:55:40 -0800

    install dependencies and update component states

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
 
-const Game = ({ decrementNumGuess }) => {
+const Game = ({ decrementNumGuess, handleGameFormChange }) => {
 
-  const handleChange = e => {
-    const numFields = 4;
-    const { maxLength, value, name } = e.target;
-    const [fieldName, fieldIndex] = name.split("-");
+  /* SUBMIT FORM */
+  const handleFormSubmit = async event => {
+    event.preventDefault();
 
-    // parseInt parses a string argument and returns the integer
-    let fieldIntIndex = parseInt(fieldIndex, 10);
-
-    // if value length is greater than max char length
-    if (value.length >= maxLength) {
-      // if not last input field
-      if (fieldIntIndex < numFields) {
-        const nextField = document.querySelector(
-          `input[name=guess-${fieldIntIndex + 1}]`
-        );
-
-        // if next field exists, focus next field
-        if (nextField !== null) {
-          nextField.focus();
-        }
-      }
-    }
-    
-  }
+  };
 
   return (
     <div>
@@ -33,11 +14,11 @@ const Game = ({ decrementNumGuess }) => {
         Guess the number combination!
       </div>
 
-      <form>
-        <input type='text' id='first' name='guess-1' maxLength={1} minLength={0} size='1' onChange={handleChange} autoFocus />
-        <input type='text' id='second' name='guess-2' maxLength={1} minLength={0} size='1' onChange={handleChange} />
-        <input type='text' id='third' name='guess-3' maxLength={1} minLength={0} size='1' onChange={handleChange} />
-        <input type='text' id='fourth' name='guess-4' maxLength={1} minLength={0} size='1' onChange={handleChange} />
+      <form onSubmit={handleFormSubmit}>
+        <input type='text' id='guess-1' name='guess-1' maxLength={1} minLength={0} size='1' onChange={handleGameFormChange} value={handleGameFormChange.guess1} autoFocus />
+        <input type='text' id='guess-2' name='guess-2' maxLength={1} minLength={0} size='1' onChange={handleGameFormChange} value={handleGameFormChange.guess2} />
+        <input type='text' id='guess-3' name='guess-3' maxLength={1} minLength={0} size='1' onChange={handleGameFormChange} value={handleGameFormChange.guess3} />
+        <input type='text' id='guess-4' name='guess-4' maxLength={1} minLength={0} size='1' onChange={handleGameFormChange} value={handleGameFormChange.guess4} />
 
         <button type='button' onClick={decrementNumGuess}>guess!</button>
       </form>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Header from '../components/Header';
 import Game from '../components/Game';
 import GuessesLeft from '../components/GuessesLeft';
 import Feedback from '../components/Feedback';
@@ -14,12 +15,47 @@ const Home = () => {
     }
   }
 
+  /* GAME FORM STATE */
+  const [gameFormState, setGameFormState] = useState({ 'guess-1': '', 'guess-2': '', 'guess-3': '', 'guess-4': '' });
+
+  // update state based on form input changes
+  const handleGameFormChange = (event) => {
+    const { maxLength, name, value } = event.target;
+
+    setGameFormState({
+      ...gameFormState,
+      [name]: value,
+    });
+
+    const numFields = 4;
+    const [fieldName, fieldIndex] = name.split('-');
+
+    // parseInt parses a string argument and returns the integer
+    let fieldIntIndex = parseInt(fieldIndex, 10);
+
+    // if value length is greater than max char length
+    if (value.length >= maxLength) {
+      // if not last input field
+      if (fieldIntIndex < numFields) {
+        const nextField = document.querySelector(
+          `input[name=guess-${fieldIntIndex + 1}]`
+        );
+
+        // if next field exists, focus next field
+        if (nextField !== null) {
+          nextField.focus();
+        }
+      }
+    }
+  };
+
 	return (
 		<div>
+      <Header />
 
-      <GuessesLeft guessesLeft={guessesLeft}/>
+      <GuessesLeft guessesLeft={guessesLeft} />
 
-      <Game decrementNumGuess={decrementNumGuess}/>
+      <Game decrementNumGuess={decrementNumGuess} handleGameFormChange={handleGameFormChange} />
 
       <Feedback />
 
